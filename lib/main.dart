21a575dec85758eea5f7034879b503074a797216
chandrafebrian <chandrafebrian99@gmail.com>
2021-05-09 23:32:13 +0700

    perubahan title sebelum nya dari chandra F jadi Chandra.F

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chandra F',
+      title: 'Chandra.F',
       debugShowCheckedModeBanner: false,
       home: Aplikasi(),
     );
